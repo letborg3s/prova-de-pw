d310c7f9f250761a284c5383da6b4c068a43ce0b
letborg3s <114627402+letborg3s@users.noreply.github.com>
2022-09-28 20:48:43 -0300

    Add files via upload

---
+++ Cal/script.js
@@ -0,0 +1,13 @@
+class calculadora{
+somar(n1,n2){return n1+n2}
+   
+sub(n1,n2){return n1-n2}
+    
+div(n1,n2){return n1/n2}
+   
+mult(n1,n2){return n1*n2}
+    
+}
+function displaynum(n1) {
+    Calcu.text1.value = Calcu.text1.value + n1;
+}
